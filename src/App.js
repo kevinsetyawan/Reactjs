b3f4962546de97b4ba45d4653ffa0e7f1cf74498
kevin <kevinsetyawan12360@outlook.com>
2020-06-22 15:21:06 +0700

    berhasil menggunakan react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 //import logo from './logo.svg';
 import './App.css';
 //import Home from './Component/Fungsional/Home';
 import BootstrapComp from './Component/Class/BootstrapComp';
+import NavbarComp from './Component/Fungsional/NavbarComp';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <BootstrapComp/>
-      </div>
+    <BrowserRouter>
+      <NavbarComp/>
+      <switch>
+         <Route exact path="/" component={HomePage}/>
+         <Route exact path="/" component={About}/>
+      </switch>
+    </BrowserRouter>
   );
 }
 
